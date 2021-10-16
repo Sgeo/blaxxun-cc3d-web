0168dc3f13b30ef2ce0d6e2bfd6d7c5233773b0d
ChrisMiuchiz <chris000005@gmail.com>
2021-10-15 21:35:18 -0400

    Fix dangling else

--- src/vrml/src/gvreaderror.cpp
+++ src/vrml/src/gvreaderror.cpp
@@ -63,12 +63,13 @@ G__cdecl GvReadError::post(GvInput *in,GvReadErrorCode errorCode, const char *fo
 
 	GReporter *reporter = in->getReporter();
 
-	if (reporter)
-	if (in->src != NULL) {
-	    reporter->Error("VRML syntax error: %s in %s:\n%s", locstr.getString(), in->src, buf );
-	}
-	else {
-		reporter->Error("VRML syntax error: %s\n%s",locstr.getString(), buf );
+	if (reporter) {
+		if (in->src != NULL) {
+		    reporter->Error("VRML syntax error: %s in %s:\n%s", locstr.getString(), in->src, buf );
+		}
+		else {
+			reporter->Error("VRML syntax error: %s\n%s",locstr.getString(), buf );
+		}
 	}
 
     //fprintf(stderr, "VRML read error: %s\n%s\n", buf, locstr.getString());
